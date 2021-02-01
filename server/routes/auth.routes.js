4475a1e50c48b26c0d34451679567f901ed8c446
Egor <zhora.filippov@list.ru>
2021-02-01 13:17:51 +0500

    Backend ready

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
 const router = Router()
 
@@ -37,8 +39,46 @@ router.post('/register', [
         res.status(500).json({message: 'Smth went wrong, try again'})
     }
 })
-router.post('/login',  async (req, res) =>{
 
-})
+router.post('/login', [
+    check('email', 'Incorrect email').normalizeEmail().isEmail(),
+    check('email', 'Enter password').exists()
+],
+    async (req, res) =>{
+        try{
+            const errors = validationResult(req)
+            if (!errors.isEmpty()){
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Incorrect email or password.'
+                })
+            }
+
+            const {email, password} = req.body
+
+           const user = await User.findOne({email})
+           if (!user){
+               res.status(400).json({message: 'This user doesn\'t exist'})
+           }
+
+           const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch){
+                res.status(400).json({message: 'Incorrect password'})
+            }
+
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret')
+            )
+
+            res.json({ token, userId : user.id})
+
+
+        } catch (e) {
+            res.status(500).json({message: 'Smth went wrong, try again'})
+        }
+    }
+    )
 
 module.exports = router
